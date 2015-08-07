8c88a61876ee5c0acbedee6bb45f5063311c2b7c
drArli <arlindo.trindade@gmail.com>
2015-08-07 17:20:16 +0100

    New Files!

--- plot3.R
+++ plot3.R
@@ -22,7 +22,7 @@ lines(subset_data$datetime, subset_data$Sub_metering_2,col="red")
 lines(subset_data$datetime, subset_data$Sub_metering_3,col="blue")
 
 
-legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black","red","blue"),cex = 0.9)
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black","red","blue"), cex=.8)
 
 
 
--- plot4.R
+++ plot4.R
@@ -19,7 +19,7 @@ lines(subset_data$datetime, subset_data$Sub_metering_2,col = "red")
 lines(subset_data$datetime, subset_data$Sub_metering_3,col = "blue")
 
 
-legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black","red","blue") , bty = "n",inset = 0.04,cex = 0.9)
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black","red","blue") , bty = "n",cex = 0.8)
 
 
 plot(subset_data$datetime, subset_data$Voltage, type="l", ylab="Voltage", xlab="datetime")
